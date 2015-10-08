4a0ad2218c5e50f339d1c9a74b9dbcb9323323f3
Roy Williams <rwilliams@lyft.com>
2015-10-08 12:34:22 -0700

    Initial commit

---
+++ git_shortcuts.sh
@@ -0,0 +1,53 @@
+# Slack Config
+export SLACK_TOKEN=$(>&2 echo "Get a slack token from https://api.slack.com/web and set it here.")
+export DEFAULT_SLACK_PR_ROOM="$(>&2 echo "Set $DEFAULT_SLACK_PR_ROOM to specify which room messages should go to by default.")
+
+# Github config
+
+if ! which hub > /dev/null; then
+  >&2 "Download the github CLI from https://github.com/github/hub"
+  exit 1
+fi
+
+function cpr() {
+  (
+    set -e
+    set -x
+    while getopts “m:c:” OPTION; do
+      case $OPTION in
+        m)
+          message="${OPTARG}"
+          ;;
+        c)
+          group="${OPTARG}"
+          ;;
+      esac
+    done
+    if [ -n "${message}" ]; then
+      if ! git diff --exit-code > /dev/null; then
+        git commit -a -m "${message}"
+      fi
+    fi
+    local message="${message-$(git log -1 --pretty=%B)}"
+    local group="${group-$DEFAULT_SLACK_PR_ROOM}"
+    git push origin $(git rev-parse --abbrev-ref HEAD) -f
+    local pr_url=$(git pull-request -m "$message")
+    echo "Pull Requset URL: ${pr_url}"
+    if which slackcli &> /dev/null && [ -n "${SLACK_TOKEN}" ]; then
+      slackcli -u "review_bot" -e ":robotface:" -g "${group}" -m "<!here|here> $USERNAME needs a code review - $pr_url ($message)"
+    fi
+  )
+}
+
+function feature() {
+  (
+    set -e
+    local feature_name=$1
+    git checkout master
+    git pull --rebase origin master
+    git checkout -b $feature_name
+  )
+}
+
+alias upr='git push origin $(git rev-parse --abbrev-ref HEAD) -f'
+alias apr='git commit -a --amend --no-edit && upr'
